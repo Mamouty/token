bd3cd457e0ec97f975c77987267dd1e797543418
Mahmoud <Mamouty4@hotmail.com>
2022-08-15 14:30:03 +0100

    Modify main.mo and Balance.jsx
    Add a public query function in the main.mo file to return the symbol and modify Balance.jsx to display the balance and its symbol to the user.

--- src/token_assets/src/components/Balance.jsx
+++ src/token_assets/src/components/Balance.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+// Importing the Principal type from the dfinity module.
+import { Principal } from '@dfinity/principal';
+import { token } from "../../../declarations/token";
 
 function Balance() {
-  
-  async function handleClick() {
-    console.log("Balance Button Clicked");
-  }
 
+  const [inputValue, setInput] = useState("");
+  const [balanceResult, setBalance] = useState("");
+
+  async function handleClick() {
+    console.log(inputValue);
+    // Creating a principle const by converting the inputValue from text to Principle.
+    const principle = Principal.fromText(inputValue);
+    //Calling the function balanceOf() from main.mo and passing the principle the user typed in the text field.
+    //Assigning the returned value from balanceOf() to a const.
+    const balance = await token.balanceOf(principle);
+    //Converting the balance to a string with JS
+    setBalance(balance.toLocaleString());
+  }
 
   return (
     <div className="window white">
@@ -15,6 +27,12 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          //To get hold of whatever the user types in the input we add the value attribute
+          value={inputValue}
+          //add an onChange listener attribute to set the input value using an arrow function
+          //e is the event that triggered the onChange, it is in our case the typing.
+          //target is the <input> and value is its value. 
+          onChange={(e) => setInput(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,9 +43,10 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p>This account has a balance of {balanceResult}.</p>
     </div>
   );
 }
 
 export default Balance;
+
